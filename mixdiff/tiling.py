82256d661a3280f77c1eaa5eded9dc9993e908f2
i-ndigo <david.list@web.de>
2024-05-18 13:05:37 +0200

    Update tiling.py
    Updated to new diffusor

--- mixdiff/tiling.py
+++ mixdiff/tiling.py
@@ -9,7 +9,7 @@ from tqdm.auto import tqdm
 from transformers import CLIPFeatureExtractor, CLIPTextModel, CLIPTokenizer
 
 from diffusers.models import AutoencoderKL, UNet2DConditionModel
-from diffusers.pipeline_utils import DiffusionPipeline
+from diffusers.pipelines.pipeline_utils import DiffusionPipeline
 from diffusers.schedulers import DDIMScheduler, PNDMScheduler
 from diffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
 from diffusers.schedulers import LMSDiscreteScheduler
